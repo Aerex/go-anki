df6b2cccadb0faee8dc283cf3f9f9ab6ee55c6f6
Aerex <noamfo@aerex.me>
2021-11-01 23:12:30 -0500

    fix(api): Override UnmarshallJSON to handle timestamp conversion on mod property

--- api/types/types.go
+++ api/types/types.go
@@ -1,5 +1,20 @@
 package types
 
 import (
-	_ "github.com/aerex/anki-cli/api/types/rest"
+	"encoding/json"
+	"time"
 )
+type UnixTime struct {
+  time.Time
+}
+
+// Override UnmarshalJSON to handle timestamp in unix format
+func (t *UnixTime) UnmarshalJSON(b []byte) error {
+	var timestamp int64
+	err := json.Unmarshal(b, &timestamp)
+	if err != nil {
+		return err
+	}
+	t.Time = time.Unix(timestamp, 0)
+	return nil
+}
