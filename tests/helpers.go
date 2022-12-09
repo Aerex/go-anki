b0bf7a4671c98a9a762b8cb5adb199c17f1b5909
Aerex <noamfo@aerex.me>
2022-12-08 21:28:07 -0500

    test: Updated table mocks

--- tests/helpers.go
+++ tests/helpers.go
@@ -2,32 +2,59 @@ package tests
 
 import (
 	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"strings"
 
 	"github.com/olekukonko/tablewriter"
+	"gopkg.in/yaml.v2"
 )
 
 type TestCmdBuffers struct {
-  InBuf *bytes.Buffer
-  OutBuf *bytes.Buffer
-  ErrBuf *bytes.Buffer
+	InBuf  *bytes.Buffer
+	OutBuf *bytes.Buffer
+	ErrBuf *bytes.Buffer
+}
+
+// Return a yaml string from a json file
+// TODO: Can't use this method because struct{} is not a given type
+// Need a way to make it generic
+func GenerateYamlOutputFromJSONFile(path string, model struct{}) (string, error) {
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+
+	err = json.Unmarshal(file, &model)
+	if err != nil {
+		return "", err
+	}
+
+	yamlData, err := yaml.Marshal(model)
+	if err != nil {
+		return "", err
+	}
+
+	return string(yamlData), nil
 }
 
 // TODO: Create a method to generate output using template
 func GenerateTableOutputWithHeaders(headers []string, rows [][]string) string {
-  tableBuf := &strings.Builder{}
-  table := tablewriter.NewWriter(tableBuf)
+	tableBuf := &strings.Builder{}
+	table := tablewriter.NewWriter(tableBuf)
 
-  table.SetHeader(headers)
-  table.SetColumnSeparator("")
-  table.SetCenterSeparator("")
-  table.SetRowSeparator("")
-  table.SetAutoFormatHeaders(false)
-  table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-  table.SetAutoWrapText(false)
+	table.SetColumnSeparator("")
+	table.SetNoWhiteSpace(true)
+	table.SetCenterSeparator("")
+	table.SetHeader(headers)
+	table.SetRowSeparator("")
+	table.SetAutoFormatHeaders(false)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetTablePadding("\t")
+	table.SetAutoWrapText(false)
 
-  table.AppendBulk(rows)
+	table.AppendBulk(rows)
 
-  table.Render()
-  return tableBuf.String()
+	table.Render()
+	return tableBuf.String()
 }
